d10beab0b5a05195b11773d041c21d2a21366e16
xRookieFight <me@xrookiefight.com>
2026-05-23 14:15:58 +0300

    chore: bump version to 0.0.1

--- php_gcprune.h
+++ php_gcprune.h
@@ -4,7 +4,7 @@
 extern zend_module_entry gcprune_module_entry;
 #define phpext_gcprune_ptr &gcprune_module_entry
 
-#define PHP_GCPRUNE_VERSION "0.1.0"
+#define PHP_GCPRUNE_VERSION "0.0.1"
 
 #ifdef PHP_WIN32
 # define PHP_GCPRUNE_API __declspec(dllexport)
